4b2579d9e701ee33e044b1e00f64a80f2582d880
Rexam14 <amerigo.mancino@gmail.com>
2016-05-04 11:28:04 +0200

    Inizializzazione package.

---
+++ FondamentiDiProbabilita.m
@@ -0,0 +1,15 @@
+(* ::Package:: *)
+
+(* :Title: FondamentiDiProbabilita *)
+
+
+(* :Context: ProgrammingInMathematica`FondamentiDiProbabilita` *)
+(* :Author: Amerigo Mancino, Andrea Segalini, Bartolomeo Lombardi *)
+(* :Summary: un pacchetto per l'introduzione al calcolo delle probabilit\[AGrave] e alla statistica per scuole superiori *)
+(* :Copyright: *)
+(* :Package Version *)
+(* :Mathematica Version: 10.3 *)
+(* :History: *)
+(* :Keywords: probabilit\[AGrave], statistica, programmazione, scuola superiore *)
+(* :Sources: biblio *)
+(* :Discussion: *)
