1670846c56b8f2783bb8487d79ff3a4556321eb2
Rexam14 <amerigo.mancino@gmail.com>
2016-05-06 10:34:01 +0200

    Aggiunte prove ripetute del lancio di un dado.

--- FondamentiDiProbabilita.m
+++ FondamentiDiProbabilita.m
@@ -14,6 +14,30 @@
 (* :Sources: biblio *)
 (* :Discussion: *)
 
-Begin["Private"]
+RollDice::usage = StringJoin[
+	"RollDice[] simula il lancio di un dado, restituendo un valore compreso fra 0 e 6.\n",
+	"RollDice[n] simula n volte il lancio di un dado, restituendo una lista di valori compresi fra 0 e 6."
+]
+
+HistogramRollDice::usage =
+	"HistogramRollDice[n] disegna un istogramma per n prove ripetute del lancio di un dado."
+
+Begin["Private`"]
+
+(* Function che simula n volte il lancio di un dado. *)
+(* Se chiamata senza parametri, il valore di default \[EGrave] 1 *)
+RollDice[n_:1] /; IntegerQ[n] && NonNegative[n] := 
+	Table[RandomChoice[{1,2,3,4,5,6}],n]
+
+(* Function che genera un grafico a barre per il lancio di un dado eseguito n volte *)
+HistogramRollDice[n_] /; IntegerQ[n] && NonNegative[n] :=
+	Module[{trials, sumTrials},
+	trials = RollDice[n];
+	sumTrials = Range[6];
+	For[i = 1, i < 7, i++,
+			sumTrials[[i]] = Count[trials, i];
+	];
+	BarChart[sumTrials, ChartLabels -> {"1","2","3","4","5","6"}, ChartElementFunction -> "GlassRectangle", ChartStyle -> "Pastel"]
+]
 
 End[]
