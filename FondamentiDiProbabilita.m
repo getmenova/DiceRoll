d30403d5bcf65adfe225fd1de95ec8c571235071
Rexam14 <amerigo.mancino@gmail.com>
2016-05-06 10:47:21 +0200

    Aggiunta function per il calcolo della probabilità di un evento.

--- FondamentiDiProbabilita.m
+++ FondamentiDiProbabilita.m
@@ -22,22 +22,32 @@ RollDice::usage = StringJoin[
 HistogramRollDice::usage =
 	"HistogramRollDice[n] disegna un istogramma per n prove ripetute del lancio di un dado."
 
+CalcoloProbabilita::usage =
+	"CalcoloProbabilita[m,n] restituisce la probabilità che un evento E accada, dati m casi favorevoli e n contrari."
+
 Begin["Private`"]
 
 (* Function che simula n volte il lancio di un dado. *)
 (* Se chiamata senza parametri, il valore di default \[EGrave] 1 *)
-RollDice[n_:1] /; IntegerQ[n] && NonNegative[n] := 
+LancioDado[n_:1] /; IntegerQ[n] && NonNegative[n] := 
 	Table[RandomChoice[{1,2,3,4,5,6}],n]
 
 (* Function che genera un grafico a barre per il lancio di un dado eseguito n volte *)
-HistogramRollDice[n_] /; IntegerQ[n] && NonNegative[n] :=
+PlotLancioDado[n_] /; IntegerQ[n] && NonNegative[n] :=
 	Module[{trials, sumTrials},
-	trials = RollDice[n];
+	trials = LancioDado[n];
 	sumTrials = Range[6];
 	For[i = 1, i < 7, i++,
 			sumTrials[[i]] = Count[trials, i];
 	];
 	BarChart[sumTrials, ChartLabels -> {"1","2","3","4","5","6"}, ChartElementFunction -> "GlassRectangle", ChartStyle -> "Pastel"]
 ]
+
+(* Calcolo della probabilità di un evento *)
+CalcoloProbabilita[favorevoli_, totali_] /; 
+	IntegerQ[favorevoli] && NonNegative[favorevoli] &&
+	IntegerQ[totali] && NonNegative[totali] && 
+	favorevoli <= totali :=
+		favorevoli/totali
 
 End[]
