bdd7d7435f2fe99747375fa8be3732698336fc30
Rexam14 <amerigo.mancino@gmail.com>
2016-06-07 16:40:30 +0200

    Modifiche alla distribuzione binomiale.

--- FondamentiDiProbabilita.m
+++ FondamentiDiProbabilita.m
@@ -342,9 +342,12 @@ ProveRipetuteSimulazione[p_Real, n_Integer, m_Integer] :=
       ];
     ];
    results[[successCounter + 1]]++;
-   ];
-  Return[results];
-  ]
+  ];
+	For[j = 1, j < Length[results], ++j,
+		results[[j]] = N[results[[j]] / m];
+	];
+	Return[results];
+ ]
 
 (* FUCTION AUSILIARIE *)
 
@@ -370,11 +373,23 @@ DadoTreD[] := Module[{fullQuad, faces},
 (* Function ausiliaria del bottone per la Probabilità classica *)
 BottoneProbabilita[] := Module[{output = 1/2},
 	Button["Probabilità Classica",
-	output=DialogInput[{favorevoli=""},
-	Column[{"Casi favorevoli",
-	InputField[Dynamic[favorevoli],Number],"Casi possibili",
-	InputField[Dynamic[possibili],Number],
-	Button["Calcola",DialogReturn[output=CalcoloProbabilita[favorevoli,possibili]]]}]],Method->"Queued"]
+		output = DialogInput[
+			{favorevoli=""},
+			Column[
+				{"Casi favorevoli",
+				 InputField[Dynamic[favorevoli],Number],"Casi possibili",
+				 InputField[Dynamic[possibili],Number],
+				 Button["Calcola", 
+				 	DialogReturn[
+						If[favorevoli > possibili,
+							 output = "Errore: i casi favorevoli non possono essere maggiori in numero dei casi possibili.",
+							 Grid[{{"Probabilità:", output = CalcoloProbabilita[favorevoli,possibili]}}]
+						]
+					]
+				]
+			 }
+			]
+		], Method->"Queued"]
 	Dynamic@output
 ]
 
